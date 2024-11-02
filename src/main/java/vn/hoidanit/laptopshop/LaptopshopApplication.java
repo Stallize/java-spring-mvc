d6642ab25bab10b86675562f11201a25abe69865
Stallize <duongtaymo2711@gmail.com>
2024-11-02 18:55:42 +0700

    mysqlconnect

--- src/main/java/vn/hoidanit/laptopshop/LaptopshopApplication.java
+++ src/main/java/vn/hoidanit/laptopshop/LaptopshopApplication.java
@@ -2,12 +2,19 @@ package vn.hoidanit.laptopshop;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+// import org.springframework.context.ApplicationContext;
 
 @SpringBootApplication
 public class LaptopshopApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(LaptopshopApplication.class, args);
+		// ApplicationContext stall = SpringApplication.run(LaptopshopApplication.class,
+		// args);
+		// for (String s : stall.getBeanDefinitionNames()) {
+		// System.out.println(s);
+		// }
+
 	}
 
 }
